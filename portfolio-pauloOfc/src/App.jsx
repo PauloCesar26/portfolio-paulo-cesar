f20be65cc4c07e9eda768c15ed3f662b4345c637
PauloCesar26 <pc.cordeirolima@gmail.com>
2025-05-14 10:53:51 -0300

    Arrumando bug

--- portfolio-pauloOfc/src/App.jsx
+++ portfolio-pauloOfc/src/App.jsx
@@ -3,7 +3,6 @@ import { Sidebar } from "@/components/ui/sidebar"
 import Layout from './components/layout'
 import './App.css'
 import { Toaster } from 'sonner'
-import './i18n/i18n'
 
 function App() {
   return (
