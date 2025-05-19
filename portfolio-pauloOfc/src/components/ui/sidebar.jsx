9e6193069d58c28888d4b6c06b82fe48c4958c6a
PauloCesar26 <pc.cordeirolima@gmail.com>
2025-05-19 20:33:55 -0300

    Ajustando erro

--- portfolio-pauloOfc/src/components/ui/sidebar.jsx
+++ portfolio-pauloOfc/src/components/ui/sidebar.jsx
@@ -216,7 +216,7 @@ function Sidebar({
         <div
           data-sidebar="sidebar"
           data-slot="sidebar-inner"
-          className="p-4 bg-zinc-300/55 text-black dark:bg-zinc-900 dark:text-white transition duration-700 ease-in-out group-data-[variant=floating]:border-sidebar-border flex h-full w-full flex-col group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:shadow-sm">
+          className="p-4 bg-zinc-300 text-black dark:bg-zinc-900 dark:text-white transition duration-700 ease-in-out group-data-[variant=floating]:border-sidebar-border flex h-full w-full flex-col group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:shadow-sm">
           {children}
         </div>
       </div>
