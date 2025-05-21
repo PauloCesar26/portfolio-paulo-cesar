5f616c9e9fad2b8efa3448036eaf176d81c3d363
PauloCesar26 <pc.cordeirolima@gmail.com>
2025-05-21 18:32:48 -0300

    Fazendo melhorias

--- portfolio-pauloOfc/src/hooks/LanguageSelect.jsx
+++ portfolio-pauloOfc/src/hooks/LanguageSelect.jsx
@@ -1,14 +1,40 @@
 import { useTranslation } from "react-i18next"
 import { useState, useEffect } from "react"
+import { LoaderCircle } from "lucide-react";
 
 function LanguageSelect() {
     const {i18n} = useTranslation();
+    const [isLoading, setIsLoading] = useState(false);
+
+    function LoandingOverlay(){
+      return (
+        <div className="
+            fixed 
+            top-0 
+            left-0
+            w-full
+            h-full
+            bg-white
+            dark:bg-black
+            opacity-90
+            z-50
+            flex
+            items-center
+            justify-center">
+              <LoaderCircle className="animate-spin w-10 h-10 text-black dark:text-white"/>
+        </div>
+      );
+    }
 
     const handleLanguage = (e) => {
       const selectLanguage = e.target.value;
 
       localStorage.setItem("idioma", selectLanguage);
-      window.location.reload();   
+      setIsLoading(true);
+
+      setTimeout(() => {
+        window.location.reload();   
+      }, 500);
     }
     const [language, setLanguage ] = useState(() => {
       return localStorage.getItem("idioma") || "pt";
@@ -16,6 +42,7 @@ function LanguageSelect() {
 
   return (
     <>
+      {isLoading && <LoandingOverlay/>}
       <div className="font flex items-center space-x-2 hover:bg-zinc-200/90 hover:dark:bg-zinc-900/60 p-2 rounded-[15px] ease-in-out transition duration-[0.3s]">
           <div className="">
             {language === "pt" ? <img src="/img/icons/pt.svg" alt="" className="w-6 h-6 object-cover rounded-full"/> : <img src="/img/icons/en.svg" alt="" className="w-5 h-5 rounded-full"/>}
